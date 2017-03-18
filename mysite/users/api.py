2b0419927c5245b217636c45347eaf5a571ac06b
Luis Miguel Álvarez <elemikelo@gmail.com>
2017-03-18 09:05:57 +0100

    Endpoint de actulizacion de usuario

--- mysite/users/api.py
+++ mysite/users/api.py
@@ -52,6 +52,21 @@ class UserDetailAPI(APIView):
         serializer = UserSerializer(user)
         return Response(serializer.data)
 
+    def put(self, request, pk):
+        """
+        Actualiza usuario
+        :param request: HttpRequest
+        :param pk: primary key del usuario
+        :return: Response
+        """
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, pk):
         """
         Borra un user

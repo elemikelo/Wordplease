528801969a174f7049dfe134c7a90a7732d36443
Luis Miguel Álvarez <elemikelo@gmail.com>
2017-03-18 09:52:15 +0100

    Validaciones para actualizar si el usuario existe

--- mysite/users/serializers.py
+++ mysite/users/serializers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 
 class UserSerializer(serializers.Serializer):
 
@@ -12,11 +14,12 @@ class UserSerializer(serializers.Serializer):
 
 
     # Datos recibidos del Post y se encarga de parsearlos
+
     def create(self, validated_data):
         return self.update(User(), validated_data)
 
     def update(self, instance, validated_data):
-        instance.firs_name = validated_data.get('first_name', instance.first_name)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
         instance.username = validated_data.get('username', instance.username)
         instance.email = validated_data.get('email', instance.email)
@@ -24,5 +27,18 @@ class UserSerializer(serializers.Serializer):
             instance.set_password(validated_data.get('password'))
         instance.save()
         return instance
+
+    def validate(self, attrs):
+        # Validar si el usuario que se intenta crear ya existe
+        if self.instance is None and User.objects.filter(username=attrs.get("username")).exists():
+            raise ValidationError("Username already exists")
+
+        # Actualizar usuario cambiando el username
+        if self.instance is not None and self.instance.username != attrs.get("username") and User.objects.filter(username=attrs.get("username")).exists():
+            raise ValidationError("Username already exists")
+
+        return attrs
+
+        # Actualizar el usuario cambiando el username, si el username no está usado
 
 
